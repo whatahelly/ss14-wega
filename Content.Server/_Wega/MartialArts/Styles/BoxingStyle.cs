abea7038fe224364948bd192e8f2c2ade48213eb
Zekins <zekins3366@gmail.com>
2025-02-21 19:06:24 +0300

    fixes

--- Content.Server/_Wega/MartialArts/Styles/BoxingStyle.cs
+++ Content.Server/_Wega/MartialArts/Styles/BoxingStyle.cs
@@ -2,6 +2,7 @@ using Content.Shared.Damage;
 using Content.Shared.Damage.Components;
 using Content.Shared.Martial.Arts;
 using Content.Shared.Martial.Arts.Components;
+using Robust.Shared.Map;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Physics.Systems;
 
@@ -11,6 +12,8 @@ namespace Content.Server.Martial.Arts;
 public sealed partial class MartialArtsSystem
 {
     [Dependency] private readonly SharedPhysicsSystem _physics = default!;
+    [Dependency] private readonly IMapManager _mapMan = default!;
+    [Dependency] private readonly SharedMapSystem _map = default!;
 
     private void InitializeBoxing()
     {
@@ -45,17 +48,24 @@ public sealed partial class MartialArtsSystem
         var transformSystem = EntityManager.System<SharedTransformSystem>();
 
         var transform = Transform(ent);
+        var entPosition = transformSystem.GetMapCoordinates(transform);
         var lookDirection = transformSystem.GetWorldRotation(transform).ToWorldVec().Normalized();
         var dodgeDirection = -lookDirection;
 
         var force = 1500f;
         if (TryComp(ent, out PhysicsComponent? physics))
         {
-            if (physics.Mass < 80f)
+            if (_mapMan.TryFindGridAt(entPosition, out _, out var grid) && grid != null)
             {
-                force *= 2;
+                if (_map.TryGetTileRef(ent, grid, transform.Coordinates, out _))
+                {
+                    if (physics.Mass < 80f)
+                    {
+                        force *= 2;
+                    }
+                    _physics.ApplyLinearImpulse(ent, dodgeDirection * force, body: physics);
+                }
             }
-            _physics.ApplyLinearImpulse(ent, dodgeDirection * force, body: physics);
         }
 
         args.Handled = true;
@@ -68,7 +78,7 @@ public sealed partial class MartialArtsSystem
         if (!TryComp<StaminaComponent>(target, out var stamina))
             return;
 
-        _stamina.TryTakeStamina(target, 5f, stamina);
+        _stamina.TryTakeStamina(target, 8f, stamina);
     }
 
     private void HandleHook(EntityUid target)
@@ -76,7 +86,7 @@ public sealed partial class MartialArtsSystem
         if (!TryComp<StaminaComponent>(target, out var stamina))
             return;
 
-        _stamina.TryTakeStamina(target, 15f, stamina);
+        _stamina.TryTakeStamina(target, 25f, stamina);
 
         if (TryComp<DamageableComponent>(target, out var damage))
         {
